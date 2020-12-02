2b1f9aa7e5f11c8d470ceeb394e5d877a433e325
Connor Tinney <connor.tinney@gmail.com>
2020-12-02 10:08:29 +0000

    Day 2

--- 2/part1.rb
+++ 2/part1.rb
@@ -4,10 +4,10 @@ passwords = CSV.parse(File.read('data.txt'), col_sep: ' ')
 count = 0
 
 passwords.each do |p|
-  position = p[0].split('-').map(&:to_i)
+  num = p[0].split('-').map(&:to_i)
   password = p[2]
   letter = p[1][0]
-  count += 1 if Range.new(position[0], position[1]) === password.count(p[1][0])
+  count += 1 if Range.new(num[0], num[1]) === password.count(p[1][0])
 end
 
 puts count
